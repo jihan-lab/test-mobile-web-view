9f75758910e856e65a131d48d43b5597645c4bce
jihan-lab <jihannabdul@gmail.com>
2023-10-18 16:04:54 +0700

    chore: add detect devide for serv 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,21 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./page.module.css";
+import { isDesktop } from "react-device-detect";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    if (isDesktop) {
+      location.replace("http://172.20.65.245:3001");
+      return;
+    }
+  });
+
   return (
     <main className={styles.main}>
-      <h1>Ini tampilan Web mobile view server 2</h1>
+      <h1>Ini tampilan Web mobile view server 1</h1>
     </main>
   );
 }

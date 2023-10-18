1a5591f81a577a62087b8fae6b58736578fa321f
jihan-lab <jihannabdul@gmail.com>
2023-10-18 16:26:31 +0700

    ..

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,14 +8,14 @@ import { useEffect } from "react";
 export default function Home() {
   useEffect(() => {
     if (isDesktop) {
-      location.replace("http://172.20.65.246:3001");
+      location.replace("http://172.20.65.245:3001");
       return;
     }
   }, []);
 
   return (
     <main className={styles.main}>
-      <h1>Ini tampilan Web mobile view server 6</h1>
+      <h1>Ini tampilan Web mobile view server 1</h1>
     </main>
   );
 }

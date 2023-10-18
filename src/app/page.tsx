14496bb08e4020acce6d9e8123dd9bf2a6a3bbea
jihan-lab <jihannabdul@gmail.com>
2023-10-18 14:01:31 +0700

    server 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>Ini tampilan Web mobile view server 1</h1>
+      <h1>Ini tampilan Web mobile view server 2</h1>
     </main>
   );
 }
